08205620ebed2ed83d5132350555c95e1d444877
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-10-26 15:18:41 +0530

    add new shell programs

--- shell/fib.sh
+++ shell/fib.sh
@@ -3,11 +3,11 @@
 read -p "Enter the number of terms: " n
 a=0
 b=1
-
-echo "Fibonacci sequence up to $n terms:"
-for (( i=0; i<n; i++ )); do
-    echo -n "$a "
+echo "$a"
+echo  "$b"
+for (( i=2; i<n; i++ )); do
     term=$((a + b))
+    echo " $term"
     a=$b
     b=$term
 done
