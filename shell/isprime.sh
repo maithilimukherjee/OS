333cf27a0852ed01cdea9399f26a9cc0aa09c8d1
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-10-26 15:01:11 +0530

    prime numbers within a range

---
+++ shell/isprime.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+isPrime() {
+    num=$1
+    if (( num < 2 )); then
+        echo "false"
+        return
+    fi
+
+    for ((i=2; i*i<=num; i++)); do
+        if (( num % i == 0 )); then
+            echo "false"
+            return
+        fi
+    done
+
+    echo "true"
+}
+
+read -p "Enter the lower bound: " l
+read -p "Enter the upper bound: " r
+
+for ((i=l; i<=r; i++)); do
+    if [[ $(isPrime $i) == "true" ]]; then
+        echo "$i is a prime number."
+    else
+        echo "$i is not a prime number."
+    fi
+done
